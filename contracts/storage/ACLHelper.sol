40d919fef5d7b8018a3d44ea59b0bd59db42b75a
Michael Andree Heuer <heuer@pc.rwth-aachen.de>
2022-01-19 13:09:38 +0100

    Added custom errors.

--- contracts/storage/ACLHelper.sol
+++ contracts/storage/ACLHelper.sol
@@ -6,14 +6,18 @@ import "../acl/ACL.sol";
 contract ACLHelper is ACL {
     bytes32 public constant STORAGE_CHANGE_ROLE = keccak256("STORAGE_CHANGE_ROLE");
 
+    error NoWritePermission();
+
     modifier onlyFromContract(address _contract){
-        require(_checkRole(address(this), _contract, STORAGE_CHANGE_ROLE, msg.data));
+        if (!_checkRole(address(this), _contract, STORAGE_CHANGE_ROLE, msg.data))
+            revert NoWritePermission();
         _;
     }
 
     modifier onlyFromTwoContracts(address _contract1, address _contract2){
-        require(_checkRole(address(this), _contract1, STORAGE_CHANGE_ROLE, msg.data)
-            || _checkRole(address(this), _contract2, STORAGE_CHANGE_ROLE, msg.data));
-        _;
+        if (!_checkRole(address(this), _contract1, STORAGE_CHANGE_ROLE, msg.data)
+            || !_checkRole(address(this), _contract2, STORAGE_CHANGE_ROLE, msg.data))
+            revert NoWritePermission();
+    _;
     }
 }
--- contracts/storage/HasStorage.sol
+++ contracts/storage/HasStorage.sol
@@ -24,29 +24,37 @@ contract HasStorage is Initializable {
         users = Users(_users);
     }
 
+    error WrongPhase(PhaseLib.Phase expected, PhaseLib.Phase actual);
+    error WrongState(DebateLib.State expected, DebateLib.State actual);
+    error WrongRole(UserLib.Role expected, UserLib.Role actual);
+    error WrongAddress(address expected, address actual);
+
     modifier onlyPhase(uint256 _debateId, PhaseLib.Phase _phase){
-        require(phases.getPhase(_debateId) == _phase);
+        if (phases.getPhase(_debateId) != _phase)
+            revert WrongPhase({expected: _phase, actual: phases.getPhase(_debateId)});
         _;
     }
     modifier excludePhase(uint256 _debateId, PhaseLib.Phase _phase){
-        require(phases.getPhase(_debateId) != _phase);
+        if (phases.getPhase(_debateId) == _phase)
+            revert WrongPhase({expected: _phase, actual: phases.getPhase(_debateId)});
         _;
     }
 
     modifier onlyArgumentState(DebateLib.Identifier memory _id, DebateLib.State _state){
-        require(debates.getArgumentState(_id) == _state);
+        if (debates.getArgumentState(_id) != _state)
+            revert WrongState({expected: _state, actual: debates.getArgumentState(_id)});
         _;
     }
 
     modifier onlyCreator(DebateLib.Identifier memory _id){
-        require(debates.getCreator(_id) == msg.sender);
+        if (msg.sender != debates.getCreator(_id))
+            revert WrongAddress({expected: debates.getCreator(_id), actual: msg.sender});
         _;
     }
 
     modifier onlyRole(uint256 _debateId, UserLib.Role _role){
-        require(users.getRole(_debateId, msg.sender) == _role);
+        if (users.getRole(_debateId, msg.sender) != _role)
+            revert WrongRole({expected: _role, actual: users.getRole(_debateId, msg.sender)});
         _;
     }
-
-
 }
