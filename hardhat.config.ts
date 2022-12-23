ac60df9c33af24a0c43571beea74cfdd52920109
Michael Heuer <michael.heuer@rwth-aachen.de>
2022-12-23 10:19:25 +0100

    Improved hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,35 +1,84 @@
-import {task} from 'hardhat/config';
-import 'hardhat-gas-reporter';
+import * as dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+
+import {HardhatUserConfig, task} from 'hardhat/config';
+import '@nomiclabs/hardhat-etherscan';
 import '@nomiclabs/hardhat-waffle';
-import '@nomiclabs/hardhat-solhint';
-import {config as dotenvConfig} from 'dotenv';
-import {resolve} from 'path';
-dotenvConfig({path: resolve(__dirname, './.env')});
+import '@typechain/hardhat';
+import 'hardhat-deploy';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
+import 'solidity-docgen';
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
+dotenv.config();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const ETH_KEY = process.env.ETH_KEY;
+const accounts = ETH_KEY ? ETH_KEY.split(',') : [];
+
+const networks = JSON.parse(
+  fs.readFileSync(path.join(__dirname, './networks.json'), 'utf8')
+);
+
+// add accounts to network configs
+for (const network of Object.keys(networks)) {
+  networks[network].accounts = accounts;
+}
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-export default {
-  solidity: '0.8.10',
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 2000
-    }
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.10',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 2000,
+      },
+      outputSelection: {
+        '*': {
+          '*': ['storageLayout'],
+        },
+      },
+    },
+  },
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      throwOnTransactionFailures: true,
+      throwOnCallFailures: true,
+      blockGasLimit: 30000000,
+      gasPrice: 8000000000,
+    },
+    ...networks,
   },
   gasReporter: {
-    currency: 'EUR',
-    //gasPrice: 30,
-    coinmarketcap: process.env.COINMARKETCAP_API_KEY
-  }
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: 'USD',
+  },
+  etherscan: {
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_KEY || '',
+      rinkeby: process.env.ETHERSCAN_KEY || '',
+      goerli: process.env.ETHERSCAN_KEY || '',
+      polygon: process.env.POLYGONSCAN_KEY || '',
+      polygonMumbai: process.env.POLYGONSCAN_KEY || '',
+      arbitrumOne: process.env.ARBISCAN_KEY || '',
+      arbitrumTestnet: process.env.ARBISCAN_KEY || '',
+    },
+  },
+  namedAccounts: {
+    deployer: 0,
+  },
+  docgen: {
+    outputDir: 'docs/core/03-reference-guide',
+    theme: 'markdown',
+    pages: 'files',
+    templates: 'docs/templates',
+    collapseNewlines: true,
+    exclude: ['test'],
+  },
 };
+
+export default config;
